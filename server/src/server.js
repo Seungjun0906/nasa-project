40e1330dcf18b9c9a6d3617715711887d5eb16e2
Seungjun <seungjun.kim0906@gmail.com>
2022-01-17 14:45:23 -0500

    resolve the security issue with mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -6,8 +6,6 @@ const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 const { loadLauncheData } = require("./models/launches.model");
 const PORT = process.env.PORT || 8000;
-const MONGO_URL =
-  "mongodb+srv://blog_project:Vja6yEDDZfXyAcXk@cluster0.n53kq.mongodb.net/nasa?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
